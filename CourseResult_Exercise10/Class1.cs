e6e602f5ac07302903668d0b72161712d2317ae2
PC <PC@DESKTOP-4L0CLK0>
2026-09-20 18:47:43 +0300

    Add project files.

---
+++ CourseResult_Exercise10/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseResult_Exercise10
+{
+    internal class CourseResult
+    {
+        
+    
+        public string StudentName { get; set; }
+        private double mark;
+        public double Mark
+        {
+            get
+
+            {
+                return mark;
+
+            }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    mark = value;
+                }
+                else
+                {
+                    Console.Write("Invalid Mark! Mark must be between 0 and 100.");
+                }
+
+
+            }
+        }
+        private string Grade
+        {
+            get
+            {
+                if (Mark <= 100 && Mark >= 85)
+                {
+                    return "A";
+                }
+                else if (Mark <= 84 && Mark >= 75)
+                {
+                    return "B";
+                }
+                else if (Mark <= 74 && Mark >= 65)
+                {
+                    return "C";
+                }
+                else if (Mark <= 64 && Mark >= 50)
+                {
+                    return "D";
+                }
+                else
+                {
+                    return "F";
+                }
+            }
+        }
+
+        public bool Passed
+        {
+            get
+            {
+                if (Mark >= 50)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+
+        }
+        public void PrintResult()
+        {
+            Console.WriteLine("=====Student Result=====");
+            Console.WriteLine("Name :" + StudentName);
+            Console.WriteLine("Mark :" + Mark);
+            Console.WriteLine("Grade :" + Grade);
+            Console.WriteLine("Passed :" + Passed);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+    
+
---
+++ CourseResult_Exercise10/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CourseResult_Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                CourseResult S = new CourseResult();
+                Console.WriteLine("=====Welcome System=====");
+                Console.Write("Enter StudentName :");
+                S.StudentName = Console.ReadLine();
+                Console.Write("Enter Mark :");
+                S.Mark = double.Parse(Console.ReadLine());
+                S.PrintResult();
+                Console.WriteLine("Testing Invalid Mark");
+                S.Mark = 120;
+
+            }
+        }
+    }
+
+}
+    
+
